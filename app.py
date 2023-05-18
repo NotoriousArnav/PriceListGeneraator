7c6de096072e3e2f738663e870d46942ceffdd00
Eliot Alderson <e.alderson@allsafe.com>
2023-05-19 01:22:20 +0530

    In a Functional State

--- app.py
+++ app.py
@@ -13,19 +13,20 @@ class Constants:
 def calculate(MRPpu, qty, constants):
     results = {}
     results['MRPpc'] = MRPpu*qty # MRP per Case
-    PtR = MRPpc/(constants.RMpercent['value']+1) # Prce to retail
-    rmargin = MRPpc-PtR # Retail margin
-    PtWS = PtR/(constants.WSpercent['value']+1) # Price to W/S
-    wsmargin = PtR-PtWS # W/S Margin
-    PtD = PtWS/(constants.DistMargin['value']+1) # Price to Distributor
-    distmargin = PtWS-PtD # Distributor Margin
-    PtSS = PtD/(constants.SSMargin['value']+1) # Price to SS
-    ssmarginn = PtD - PtSS # SS Margin
-    netrate = PtSS/(constants.GST['value']+1) # Net Rate
-    GST = PtD - netrate # GST
-    basic_price = netrate/constants.basic_price['value'] # Basic Price
-    importduty = basic_price/(constants.IDpercent['value']+1) # Import Duties
-    td = basic_price/(constants.TDpercent+1) # Trade Discount
+    results['PtR'] = results['MRPpc']/(constants.RMpercent['value']+1) # Prce to retail
+    results['rmargin'] = results['MRPpc']-results['PtR'] # Retail margin
+    results['PtWS'] = results['PtR']/(constants.WSPercent['value']+1) # Price to W/S
+    results['wsmargin'] = results['PtR']-results['PtWS'] # W/S Margin
+    results['PtD'] = results['PtWS']/(constants.DistMargin['value']+1) # Price to Distributor
+    results['distmargin'] = results['PtWS']-results['PtD'] # Distributor Margin
+    results['PtSS'] = results['PtD']/(constants.SSMargin['value']+1) # Price to SS
+    results['ssmarginn'] = results['PtD'] - results['PtSS'] # SS Margin
+    results['netrate'] = results['PtSS']/(constants.GST['value']+1) # Net Rate
+    results['GST'] = results['PtD'] - results['netrate'] # GST
+    results['basic_price'] = results['netrate']/constants.basic_price['value'] # Basic Price
+    results['importduty'] = results['basic_price']/(constants.IDpercent['value']+1) # Import Duties
+    results['td'] = results['basic_price']/(constants.TDpercent['value']+1) # Trade Discount
+    return results
 
 if __name__ == "__main__":
     constants = Constants('consts.json')
